9fdd1a6d6172e7e31742b4dc650444ba942701cb
vladi <vladi@petrovlad_vm1>
2021-05-11 20:38:26 +0300

    deleting wrong files x2

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo -E -u mongo -H bash -c "export PATH=\"/apps/mongo${PATH:+:${PATH}}\"; echo $PATH"
-sudo -E -u mongo -H bash -c "echo $PATH"
-sudo -E -u mongo echo $PATH
-echo $PATH
-
